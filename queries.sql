49ecfc5753257b38000ca399d647c6587f5c8558
manofsimplepleasures <147225364+manofsimplepleasures@users.noreply.github.com>
2025-03-21 14:32:09 +0700

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,113 +1,69 @@
-customers_count.csv
+/* считает общее количество покупателей в таблице customers*/
+SELECT COUNT(DISTINCT customer_id) AS customers_count
+FROM customers;
 
-	SELECT COUNT(*) AS customers_count FROM customers;
-	общее кол-во покупателей в таблице customers
-	
-	
-Анализ отдела продаж:
-	
-top_10_total_income.csv
+/* выводит данные продавца, суммарной выручки с проданных товаров
+и количество проведенных сделок.Сортирует по убыванию выручки*/
+SELECT
+    CONCAT(e.first_name, ' ', e.last_name) AS seller,
+    COUNT(s.sales_id) AS operations,
+    FLOOR(SUM(p.price * s.quantity)) AS income
+FROM
+    sales AS s
+INNER JOIN
+    employees AS e ON s.sales_person_id = e.employee_id
+INNER JOIN
+    products AS p ON s.product_id = p.product_id
+GROUP BY
+    e.first_name, e.last_name
+ORDER BY
+    income DESC
+LIMIT 10;
 
-	SELECT CONCAT(first_name,' ',last_name) as seller,
-	склеивает имя, пробел, фамилия и выводит как seller
+/* выводит данные инфy о продавцах, чья средняя выручка за сделку < средней выручки за сделку по всем продавцам.
+ сортирует по выручке, по возрастанию.*/
+WITH seller_avg_income AS (
+    SELECT
+        CONCAT(e.first_name, ' ', e.last_name) AS seller,
+        FLOOR(SUM(p.price * s.quantity) / COUNT(s.sales_id)) AS average_income
+    FROM sales AS s
+    INNER JOIN employees AS e ON s.sales_person_id = e.employee_id
+    INNER JOIN products AS p ON s.product_id = p.product_id
+    GROUP BY seller
+),
 
-	COUNT(sales.sales_id) AS operations,
-	считает продажи по id и выводит как operations
+overall_avg_income AS (
+    SELECT FLOOR(AVG(average_income)) AS avg_income FROM seller_avg_income
+)
 
-	FLOOR(SUM(p.price * sales.quantity)) AS income
-	умножает стоимость продукта на количество продаж, суммирует и округляет до целого числа 
-	и выводит как income
-    
-	JOIN employees e  ON sales.sales_person_id = e.employee_id 
-	соединяет таблицу sales с таблицей employees,
-	связывая sales.sales_person_id с employees.employee_id
+SELECT
+    seller_avg_income.seller,
+    seller_avg_income.average_income
+FROM seller_avg_income
+INNER JOIN
+    overall_avg_income
+    ON seller_avg_income.average_income < overall_avg_income.avg_income
+ORDER BY seller_avg_income.average_income ASC;
 
-	JOIN products p ON sales.product_id = p.product_id
-	соединяет таблицу sales с таблицей products,
-	связывая sales.sproduct_id с products.product_id
+/*выводит инфу о выручке по дням недели. содержит имя,фамилию продавца, день недели и суммарную выручку. 
+ сортирует данные по порядковому номеру дня недели и seller*/
+WITH seller_income AS (
+    SELECT
+        CONCAT(e.first_name, ' ', e.last_name) AS seller,
+        FLOOR(SUM(p.price * s.quantity)) AS income,
+        TO_CHAR(s.sale_date, 'ID') AS day_number,
+        TO_CHAR(s.sale_date, 'Day') AS day_of_week
+    FROM sales AS s
+    INNER JOIN employees AS e ON s.sales_person_id = e.employee_id
+    INNER JOIN products AS p ON s.product_id = p.product_id
+    GROUP BY seller, sale_date
+)
 
-	GROUP BY seller
-	Группирует данные по продавцу
+SELECT
+    seller,
+    day_of_week,
+    income
+FROM seller_income
+GROUP BY seller, day_of_week, day_number, income
+ORDER BY day_number, income;
 
-	ORDER BY income desc
-	cортирует результат по убыванию DESC колонки income (общая сумма выручки продавца)
-
-	LIMIT 10;
-	ограничивает результат 10 строками 
-
-	
-lowest_average_income
-
-	Общее кол-во сделок на продавца
-	SELECT sales_person_id, COUNT(sales_id) AS total_deals
-	FROM sales
-	GROUP BY sales_person_id;
-
-	Общий доход на продавца
-	SELECT 
-    s.sales_person_id,
-    SUM(p.price * s.quantity) AS total_income
-	FROM sales s
-	JOIN products p ON s.product_id = p.product_id
-	GROUP BY s.sales_person_id;
-
-	Средняя выручка за сделку /продавца
-	SELECT 
-	    s.sales_person_id,
-	    FLOOR(SUM(p.price * s.quantity) / COUNT(s.sales_id)) AS average_income
-	FROM sales s
-	JOIN products p ON s.product_id = p.product_id
-	GROUP BY s.sales_person_id;
-
-	Подтягиваю «Имя», « », «Фамилия» продавцов
-	SELECT 
-	    CONCAT(e.first_name, ' ', e.last_name) AS seller,
-	    FLOOR(SUM(p.price * s.quantity) / COUNT(s.sale_id)) AS average_income
-	FROM sales s
-	JOIN employees e ON s.sales_person_id = e.employee_id
-	JOIN products p ON s.product_id = p.product_id
-	GROUP BY seller;
-	
-	Запрос WITH overall_avg_income, чтобы вычислить среднюю выручку всех продавцов. 
-	И выяснить, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам:
-	WITH seller_avg_income AS 
-		(SELECT 
-	        CONCAT(e.first_name, ' ', e.last_name) AS seller,
-	        FLOOR(SUM(p.price * s.quantity) / COUNT(s.sales_id)) AS average_income
-	    FROM sales s
-	    JOIN employees e ON s.sales_person_id = e.employee_id
-	    JOIN products p ON s.product_id = p.product_id
-	    GROUP BY seller),
-		overall_avg_income AS 
-		(SELECT FLOOR(AVG(average_income)) AS avg_income FROM seller_avg_income)
-	SELECT seller_avg_income.seller, seller_avg_income.average_income
-	FROM seller_avg_income 
-	JOIN overall_avg_income ON seller_avg_income.average_income < overall_avg_income.avg_income
-	ORDER BY seller_avg_income.average_income ASC;	
-	
-day_of_the_week_income
-
-	считаю и округляю выручку /продавца
-	FLOOR(SUM(p.price * s.quantity)) AS income
-	
-	извлекаю из sale_date номер дня недели в формате ISO (понедельник = 1, воскресенье = 7)
-	извлекаю название дня недели
-	TO_CHAR(s.sale_date, 'ID') AS day_number,
-	TO_CHAR(s.sale_date, 'Day') AS day_of_week
-	 
-	с помощью WITH seller_income AS получаю и группирую данные по выручке, по каждому продавцу и дню недели.
-	далее вывожу нужные поля, сортирую по порядковому номеру дня недели и seller
-	WITH seller_income AS 
-			(SELECT 
-		    CONCAT(e.first_name, ' ', e.last_name) AS seller,
-		    FLOOR(SUM(p.price * s.quantity)) AS income,
-	        TO_CHAR(s.sale_date, 'ID') AS day_number,
-			TO_CHAR(s.sale_date, 'Day') AS day_of_week
-		    FROM sales s
-		    JOIN employees e ON s.sales_person_id = e.employee_id
-		    JOIN products p ON s.product_id = p.product_id
-		    GROUP BY seller, sale_date)
-		SELECT seller, day_of_week, income
-		FROM seller_income 
-		GROUP by seller,day_of_week,day_number,income
-		ORDER BY day_number, income;
