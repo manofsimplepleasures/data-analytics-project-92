2bd553b4c0b82adbdbf4ee383ebd123710d78685
manofsimplepleasures <147225364+manofsimplepleasures@users.noreply.github.com>
2025-04-04 01:14:54 +0700

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -112,21 +112,31 @@ ORDER BY selling_month;
 WITH first_sale_date AS (
     SELECT
         customer_id,
-        MIN(sale_date) AS sale_date
+        MIN(sale_date) AS first_date
     FROM sales
     GROUP BY customer_id
 ),
 
-promo_first_buyers AS (
+first_day_sales AS (
     SELECT
         s.customer_id,
         s.sale_date,
-        s.sales_person_id
-    FROM sales AS s
-    JOIN products AS p ON s.product_id = p.product_id
-    JOIN first_sale_date fsd ON s.customer_id = fsd.customer_id
-                             AND s.sale_date = fsd.sale_date
-    WHERE p.price = 0
+        s.product_id,
+        s.sales_person_id,
+        p.price
+    FROM sales s
+    JOIN first_sale_date fsd
+      ON s.customer_id = fsd.customer_id AND s.sale_date = fsd.first_date
+    JOIN products p ON s.product_id = p.product_id
+),
+
+promo_first_buyers AS (
+    SELECT DISTINCT ON (customer_id)
+        customer_id,
+        sale_date,
+        sales_person_id
+    FROM first_day_sales
+    WHERE price = 0
 )
 
 SELECT
